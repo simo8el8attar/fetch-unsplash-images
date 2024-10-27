23f4a053430543c3baec83874ed561869107884e
EL ATTAR SIMO <simo4life1173@gmail.com>
2024-10-27 12:38:44 +0100

    Fetch Unsplash Images with unsplash api

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,52 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import Images from './images'
+import unsplash from './Unsplash';
+
+
 function App() {
-  const [imgCounter , setImageCounter] = useState(0);
-
-  const [imgWidth , setImgWidth] = useState('1050');
-  const [imgHeight , setImgHeight] = useState('300');
-
-  const imgStyle = {
-    width : `${imgWidth}px`,
-    height : `${imgHeight}px`
-  };
-  const nextImg = () =>{
-    Images.length - 1 > imgCounter ? setImageCounter(imgCounter + 1) : setImageCounter(0);
-    console.log(imgCounter)
-  };
-  const prevImg = () =>{
-    imgCounter > 0 ? setImageCounter(imgCounter - 1) : setImageCounter(Images.length - 1);
+  const [imgs,setImgs] = useState([]);
+  const [imgQuery , setImgQuery] = useState(null);
+  const [numberOfImgs , setNumberOfImgs] = useState(5);
+  const fetchImages = async () => {
+    try{
+      const response = await unsplash.search.getPhotos(
+        {
+        query : imgQuery,
+        page: 1,
+        perPage: numberOfImgs
+        }
+      );
+      const data = response.response;
+      console.log(data);
+      setImgs(data.results);
+    }
+    catch(err){
+      console.log(err);
+    }
   }
   return (
     <>
-      <h1>Image Caroussel</h1>
+      <h1>Search for unslash images</h1>
       <hr />
-      <div>
-        Adjust Image :
-        <p>
-          <strong>select Width :  </strong>
-            <input
-              onChange={(e)=>{setImgWidth(e.target.value)}}
-             type="text" placeholder={`${imgWidth}px`}/><br/><br/>
-         <strong>select Height : </strong> 
-              <input
-               onChange={(e)=>{setImgHeight(e.target.value)}}
-               type="text" placeholder={`${imgHeight}px`} />
-        </p>
-      </div>
-      <div className='container d-flex flex-column'>
-        <img style={imgStyle} src={Images[imgCounter]}/>
-        <br/>
-        <button onClick={nextImg} className='btn btn-primary'>Next</button>
-        <br/>
-        <button onClick={prevImg} className='btn btn-danger'>Prev</button>
-      </div>
-    </>
+      
+      <input className='input' type="text" onChange={(e)=>{setImgQuery(e.target.value)}}/>
+    <p>
+      <strong>How many Pictures :</strong>
+      <select onChange={(e)=>{setNumberOfImgs(e.target.value)}}>
+        <option value="5">5</option>
+        <option value="10">10</option>
+      </select>
+      </p>
+    
+      
+      <button className='btn btn-primary m-1' onClick={fetchImages}>fetch images</button>
+        {imgs.map((i,index)=>(
+          <div key={index}>
+            <img src={i.urls.small} alt={i.slug} /><br/>
+            <strong>{i.slug}</strong>
+            </div>))}
+      
+      </>
   )
 }
 
---
+++ src/Unsplash.js
@@ -0,0 +1,6 @@
+import { createApi } from 'unsplash-js';
+const unsplash = createApi({
+    accessKey: 'o3tWBf_0pSlOeOQvCZdfUIMp0Ei3aLpjI2AMIR6nMC0',
+  });
+
+  export default unsplash;
